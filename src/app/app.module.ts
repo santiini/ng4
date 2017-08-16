ff0279987cc7c1306f0657caf6970753d0fdec4d
zzz <865077695@qq.com>
2017-08-16 15:27:48 +0800

    清空

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,28 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { ParentAndChildComponent } from './parent-and-child/parent-and-child.component';
-import { ParentComponent } from './parent/parent.component';
-import { TestChildComponent } from './parent/test-child/test-child.component';
-import { BrotherComComponent } from './brother-com/brother-com.component';
-import { Child1Component } from './brother-com/child1/child1.component';
-import { Child2Component } from './brother-com/child2/child2.component';
 
-import { EventBusService } from './service/event-bus.service';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ParentComponent,
-    TestChildComponent,
-    BrotherComComponent,
-    Child1Component,
-    Child2Component
+    AppComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [EventBusService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
