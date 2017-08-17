7384357447f278928f5259035f387969990ead77
zzz <865077695@qq.com>
2017-08-17 10:11:59 +0800

    清空

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
-import { TestFlyInComponent } from './test-fly-in/test-fly-in.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    TestFlyInComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
