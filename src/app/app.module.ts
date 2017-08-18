4090be15505b4100afd131c4e3cad42afa476754
zzz <865077695@qq.com>
2017-08-18 14:36:45 +0800

    模板、组件类、元数据加注释

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,15 +7,15 @@ import { TestFlyInComponent } from './test-fly-in/test-fly-in.component';
 
 
 @NgModule({
-  declarations: [
+  declarations: [         // 声明本模块中拥有的视图类。Angular 有三种视图类：组件、指令和管道。
     AppComponent,
     TestFlyInComponent
   ],
-  imports: [
+  imports: [              // 本模块声明的组件模板需要的类所在的其它模块。
     BrowserModule,
     BrowserAnimationsModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [],          // 服务的创建者，并加入到全局服务列表中，可用于应用任何部分
+  bootstrap: [AppComponent] //  指定应用的主视图（称为根组件），它是所有其它视图的宿主。只有根模块才能设置bootstrap属性
 })
 export class AppModule { }
--- src/app/test-fly-in/test-fly-in.component.ts
+++ src/app/test-fly-in/test-fly-in.component.ts
@@ -1,12 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { flyIn } from '../animations/fly-in';
-
+// 模板、元数据和组件类共同构成一个模块
+// 使用装饰器向你的代码中添加元数据。以便Angular知道他该做什么
+// 元数据
 @Component({
   selector: 'app-test-fly-in',
-  templateUrl: './test-fly-in.component.html',
+  templateUrl: './test-fly-in.component.html',  // 模板
   styleUrls: ['./test-fly-in.component.css'],
   animations: [flyIn]
 })
+
+// 组件类
 export class TestFlyInComponent implements OnInit {
   state: boolean = true;
   constructor() { }
