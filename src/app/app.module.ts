f9cc811d40e1a8c1e6eb6551dd29b1a3ebfb91f4
zhiq q <865077695@qq.com>
2017-08-20 13:13:55 +0800

    最基础的路由写法

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { JokesComponent } from './jokes/jokes.component';
+import { appRoutes } from './app.routes';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    JokesComponent
   ],
   imports: [
-    BrowserModule, FormsModule
+    BrowserModule, RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
