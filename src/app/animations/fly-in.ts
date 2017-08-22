6c50e16e0983ed9715011435a5675e4eae43e974
zhiq <865077695@qq.com>
2017-08-22 10:54:58 +0800

    测试使用ng-Zorro-antd

--- src/app/animations/fly-in.ts
+++ src/app/animations/fly-in.ts
@@ -2,13 +2,13 @@ import { trigger, state, style, transition, animate, keyframes } from '@angular/
 
 export const flyIn = trigger('flyIn', [
   transition('void => *', [
-    animate(3000, keyframes([
+    animate(1000, keyframes([
       style({ opacity: 0, transform: 'translateX(-100%)' }),
       style({ opacity: 1, transform: 'translateX(0)' })
     ]))
   ]),
   transition('* => void', [
-    animate(3000, keyframes([
+    animate(1000, keyframes([
       style({ opacity: 1, transform: 'translateX(0)' }),
       style({ opacity: 0, transform: 'translateX(100%)' })
     ]))
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { NgZorroAntdModule } from 'ng-zorro-antd';
 import { AppComponent } from './app.component';
 import { TestFlyInComponent } from './test-fly-in/test-fly-in.component';
 
 
 @NgModule({
-  declarations: [         // 声明本模块中拥有的视图类。Angular 有三种视图类：组件、指令和管道。
+  declarations: [
     AppComponent,
     TestFlyInComponent
   ],
-  imports: [              // 本模块声明的组件模板需要的类所在的其它模块。
+  imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    FormsModule,
+    HttpModule,
+    BrowserAnimationsModule,
+    NgZorroAntdModule.forRoot()
   ],
-  providers: [],          // 服务的创建者，并加入到全局服务列表中，可用于应用任何部分
-  bootstrap: [AppComponent] //  指定应用的主视图（称为根组件），它是所有其它视图的宿主。只有根模块才能设置bootstrap属性
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
