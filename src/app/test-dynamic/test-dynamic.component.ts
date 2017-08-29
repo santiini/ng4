2f81ffb6d5f666bc06e203cedf5aa5d39d2524a6
zhiq <865077695@qq.com>
2017-08-29 19:10:16 +0800

    learn 动态组件

---
+++ src/app/test-dynamic/test-dynamic.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-test-dynamic',
+  templateUrl: './test-dynamic.component.html',
+  styleUrls: ['./test-dynamic.component.css']
+})
+export class TestDynamicComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
